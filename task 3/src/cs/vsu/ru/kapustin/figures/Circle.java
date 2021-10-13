5e8d563f7fdd0878ac8c93f9ad2b1871b9612608
Максим Капустин <maximkapus7in@yandex.ru>
2021-10-13 15:49:50 +0300

    Update Circle.java

--- task 3/src/cs/vsu/ru/kapustin/figures/Circle.java
+++ task 3/src/cs/vsu/ru/kapustin/figures/Circle.java
@@ -1,9 +1,9 @@
 package cs.vsu.ru.kapustin.figures;
 
 public class Circle {
-    public double x0;
-    public double y0;
-    public double r;
+    double x0;
+    double y0;
+    double r;
 
     public Circle(double x0, double y0, double r) {
         this.x0 = x0;
